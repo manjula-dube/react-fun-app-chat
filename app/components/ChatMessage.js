e1d6f529f9822ce39f2c9f478ffb43b2885fa706
Manjula Dube <dube.manjula668@gmail.com>
2018-06-17 18:45:37 +0530

    chat message added

---
+++ app/components/ChatMessage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { chatData } from '../api/apiConfig'
+
+class ChatMessage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       message:''
+    }
+  }
+
+
+  render() {
+    
+    return (
+      <div style={{width:'60%'}}>
+      <div>djqi</div>
+      
+         
+      </div>
+    );
+  }
+}
+
+export default ChatMessage;
--- app/components/TeamChat.js
+++ app/components/TeamChat.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import { chatData } from '../api/apiConfig'
+import ChatMessage from './ChatMessage'
+import _ from 'lodash'
+
 
 class TeamChat extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-       data: [],
+       chatdata: [],
+       done: false
     }
   }
 
   componentDidMount() {
+
     fetch(chatData)
-      .then( (response) => {
-         this.setState({ data: response});
-      } )  
+      .then(response => response.json())
+      .then(data => this.setState({ chatdata: data }));
   }
 
   render() {
+    const request_chat = []
+    let unique_chat = []
+    const unique_requested = this.state.chatdata.map(chat => {
+       request_chat.push(chat.requested_by)
+       unique_chat = _.uniqBy(request_chat)
+    })
+    
+    //we can have a unique chat
+    console.log(unique_chat)
+
     return (
-      <div>
-          <div className="chat-requester"> gggg</div>
-      </div>
+       <React.Fragment>
+        <div style={{width:'40%',display:'inline-block'}}>
+        {this.state.chatdata.map(chat =>
+            <div key={chat.id}>
+              <ul>
+              <li style={{width:'300px', height: '40px', background: '#EAF7FF'}}> {chat.requested_by}</li>
+              </ul>
+            </div>
+          )}
+        </div>
+       </React.Fragment>
     );
   }
 }
