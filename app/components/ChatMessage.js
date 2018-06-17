8d8ff491eb93cd02898df80577c9b87b736bae5c
Manjula Dube <dube.manjula668@gmail.com>
2018-06-17 19:49:39 +0530

    added initial messages

--- app/components/ChatMessage.js
+++ app/components/ChatMessage.js
@@ -14,7 +14,7 @@ class ChatMessage extends React.Component {
   render() {
     console.log(this.props.chat)
     return (
-      <div style={{width:'60%',float:'left',padding:'10px'}}>
+      <div style={{width:'60%',float:'left',padding:'10px',height:'600px',overflowY: 'scroll'}}>
         {this.props.chat.map(chat =>
             <div key={chat.id}>
               <div style={{width:'100%', height: '50px', border: '1px solid black',background: '#F5F6F7',margin: '12px'}}> {chat.initial_message}</div>
